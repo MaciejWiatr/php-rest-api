98639074a3de649a0c7ea4414eb0b38f59542145
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-04-25 23:20:11 +0200

    Add presentation

--- index.php
+++ index.php
@@ -3,12 +3,10 @@ require_once "router.php";
 require_once "db/DatabaseConnection.php";
 require_once "controllers/BookController.php";
 require_once "repositories/BookRepository.php";
-require_once "controllers/UserController.php";
 
 $bookRepository = new BookRepository($connection);
 
 Router::register('/book', new BookController($bookRepository));
-Router::register('/user', new UserController());
 
 Router::enableJson();
 Router::run();
